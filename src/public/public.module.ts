f9b47201e21c9b1e75b6bead4754ff0184f66b70
SANUSI2002 <69240534+SANUSI2002@users.noreply.github.com>
2026-07-11 22:54:36 +0100

    Add SettingsModule to PublicModule imports

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -3,9 +3,10 @@ import { PublicController } from './public.controller';
 import { ProductsModule } from '../products/products.module';
 import { FeaturesModule } from '../features/features.module';
 import { ModulesModule } from '../modules/modules.module';
+import { SettingsModule } from '../settings/settings.module';
 
 @Module({
-  imports: [ProductsModule, FeaturesModule, ModulesModule],
+  imports: [ProductsModule, FeaturesModule, ModulesModule, SettingsModule],
   controllers: [PublicController],
 })
 export class PublicModule {}
