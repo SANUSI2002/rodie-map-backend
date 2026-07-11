a13d4cecc7d83a865738529c70da127838dba221
SANUSI2002 <69240534+SANUSI2002@users.noreply.github.com>
2026-07-11 20:08:48 +0100

    Update public.module.ts

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
 import { ProductsModule } from '../products/products.module';
 import { FeaturesModule } from '../features/features.module';
+import { ModulesModule } from '../modules/modules.module';
 
 @Module({
-  imports: [ProductsModule, FeaturesModule],
+  imports: [ProductsModule, FeaturesModule, ModulesModule],
   controllers: [PublicController],
 })
 export class PublicModule {}
